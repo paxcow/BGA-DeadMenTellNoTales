b28c63d541a25a96c764362a5249545a808997bf
Andrea Paxcow Vitaglianom <andrea.vitagliano@gmail.com>
2025-11-01 18:43:09 -0500

    Add room, revenge, token, and item data structures for Dead Men Pax game

---
+++ modules/php/material.inc.php
@@ -0,0 +1,566 @@
+<?php
+
+namespace Bga\Games\deadmenpax;
+
+$this->roomsData = [
+    [
+        "type" => "starting_room",
+        "fireColor" => "yellow",
+        "startFireValue" => null,
+        "doors" => [
+            "north" => true,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null,
+        "posX" => 0,
+        "posY" => 0
+
+    ],
+    [
+        "type" => "starting_room",
+        "fireColor" => "red",
+        "startFireValue" => null,
+        "doors" => [
+            "north" => true,
+            "south" => false,
+            "east" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null,
+        "posX" => -1,
+        "posY" => 0
+    ],
+    [
+        "type" => "starting_room",
+        "fireColor" => "yellow",
+        "startFireValue" => null,
+        "doors" => [
+            "north" => true,
+            "south" => false,
+            "east" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null,
+        "posX" => 0,
+        "posY" => 1
+    ],
+    [
+        "type" => "starting_room",
+        "fireColor" => "red",
+        "startFireValue" => null,
+        "doors" => [
+            "north" => true,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null,
+        "posX" => 1,
+        "posY" => 0
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 3,
+        "doors" => [
+            "north" => true,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 5
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 4,
+        "doors" => [
+            "north" => true,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 4,
+        "doors" => [
+            "north" => true,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 2,
+        "doors" => [
+            "north" => false,
+            "east" => false,
+            "south" => true,
+            "west" => false
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 4
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 4,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => true,
+            "west" => false
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 1,
+        "doors" => [
+            "north" => false,
+            "east" => false,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 3
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 2,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => true,
+            "west" => false
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 3,
+        "doors" => [
+            "north" => false,
+            "east" => false,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 5
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 1,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 3
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 1,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 2,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 4
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 3,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => false,
+            "west" => false
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 0,
+        "doors" => [
+            "north" => true,
+            "east" => false,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 2
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 3,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 4,
+        "doors" => [
+            "north" => true,
+            "east" => true,
+            "south" => true,
+            "west" => false
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 4,
+        "doors" => [
+            "north" => true,
+            "east" => true,
+            "south" => false,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 2,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => false,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "red",
+        "startFireValue" => 0,
+        "doors" => [
+            "north" => true,
+            "east" => false,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => "south",
+        "kegThreshold" => 2
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 2,
+        "doors" => [
+            "north" => true,
+            "east" => false,
+            "south" => true,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ],
+    [
+        "type" => "room",
+        "fireColor" => "yellow",
+        "startFireValue" => 3,
+        "doors" => [
+            "north" => false,
+            "east" => true,
+            "south" => false,
+            "west" => true
+        ],
+        "kegDoor" => null,
+        "kegThreshold" => null
+    ]
+
+
+];
+
+$this->revengeData = [
+    [
+        "color" => "both",
+        "threshold" => 0,
+        "effect" => "crew"
+    ],
+
+    [
+        "color" => "both",
+        "threshold" => 1,
+        "effect" => "crew"
+    ],
+    [
+        "color" => "both",
+        "threshold" => 2,
+        "effect" => "crew"
+    ],
+    [
+        "color" => "both",
+        "threshold" => 3,
+        "effect" => "crew"
+    ],
+    [
+        "color" => "both",
+        "threshold" => 4,
+        "effect" => "add"
+    ],
+    [
+        "color" => "both",
+        "threshold" => 5,
+        "effect" => null
+    ],
+    [
+        "color" => "both",
+        "threshold" => 5,
+        "effect" => null
+    ],
+    [
+        "color" => "red",
+        "threshold" => 0,
+        "effect" => "add"
+    ],
+    [
+        "color" => "red",
+        "threshold" => 1,
+        "effect" => "add"
+    ],
+    [
+        "color" => "red",
+        "threshold" => 2,
+        "effect" => "spread"
+    ],
+    [
+        "color" => "red",
+        "threshold" => 3,
+        "effect" => "spread"
+    ],
+    [
+        "color" => "red",
+        "threshold" => 4,
+        "effect" => "spread"
+    ],
+    [
+        "color" => "red",
+        "threshold" => 5,
+        "effect" => null
+    ],
+    [
+        "color" => "yellow",
+        "threshold" => 0,
+        "effect" => "add"
+    ],
+    [
+        "color" => "yellow",
+        "threshold" => 1,
+        "effect" => "add"
+    ],
+    [
+        "color" => "yellow",
+        "threshold" => 2,
+        "effect" => "spread"
+    ],
+    [
+        "color" => "yellow",
+        "threshold" => 3,
+        "effect" => "spread"
+    ],
+    [
+        "color" => "yellow",
+        "threshold" => 4,
+        "effect" => "spread"
+    ],
+    [
+        "color" => "yellow",
+        "threshold" => 5,
+        "effect" => null
+    ],
+];
+
+$this->tokenData = [
+    [
+        "front" => [
+            "type" => "crew",
+            "value" => 3
+        ],
+        "back" => [
+            "type" => "grog",
+            "value" => 4
+        ],
+        "nbr" => 2
+    ],
+    [
+        "front" => [
+            "face" => "crew",
+            "value" => 3,
+        ],
+        "back" => [
+            "face" => "cutlass",
+            "value" => null,
+        ],
+        "nbr" => 1,
+    ],
+    [
+        "front" => [
+            "face" => "crew",
+            "value" => 4,
+        ],
+        "back" => [
+            "face" => "cutlass",
+            "value" => null,
+        ],
+        "nbr" => 2,
+    ],
+    [
+        "front" => [
+            "face" => "crew",
+            "value" => 4,
+        ],
+        "back" => [
+            "face" => "grog",
+            "value" => 5,
+        ],
+        "nbr" => 1,
+    ],
+    [
+        "front" => [
+            "face" => "crew",
+            "value" => 5,
+        ],
+        "back" => [
+            "face" => "cutlass",
+            "value" => null,
+        ],
+        "nbr" => 2,
+    ],
+    [
+        "front" => [
+            "face" => "guardian",
+            "value" => 6,
+        ],
+        "back" => [
+            "face" => "treasure",
+            "value" => null,
+        ],
+        "nbr" => 2,
+    ],
+    [
+        "front" => [
+            "face" => "guardian",
+            "value" => 7,
+        ],
+        "back" => [
+            "face" => "treasure",
+            "value" => null,
+        ],
+        "nbr" => 2,
+    ],
+    [
+        "front" => [
+            "face" => "guardian",
+            "value" => 8,
+        ],
+        "back" => [
+            "face" => "treasure",
+            "value" => null,
+        ],
+        "nbr" => 2,
+    ],
+    [
+        "front" => [
+            "face" => "trap",
+            "value" => "null",
+        ],
+        "nbr" =>  6,
+    ],
+
+];
+
+$this->itemData = [
+    [
+        "name" => clienttranslate("Blanket"),
+        "text" => clienttranslate("May lower a Fire Die by 2 once per turn."),
+        "cost" => 1
+    ],
+    [
+        "name" => clienttranslate("Bucket"),
+        "text" => clienttranslate("May lower a Fire Die in an adjacent room once per turn."),
+        "cost" => 1
+    ],
+    [
+        "name" => clienttranslate("Compass"),
+        "text" => clienttranslate("One free Walk or Run Action per turn."),
+        "cost" => 0
+    ],
+    [
+        "name" => clienttranslate("Dagger"),
+        "text" => clienttranslate("One free Eliminate Deckhand Action per turn."),
+        "cost" => 0
+    ],
+    [
+        "name" => clienttranslate("Pistol"),
+        "text" => clienttranslate("Nay attack from an adjacent room for on Action once per turn. No fatigue is lost for a failed attack."),
+        "cost" => 1
+    ],
+    [
+        "name" => clienttranslate("Rum"),
+        "text" => clienttranslate("One free Rest Action per turn."),
+        "cost" => 0
+    ],
+    [
+        "name" => clienttranslate("Sword"),
+        "text" => clienttranslate("Add 1 to Strength in Battle."),
+        "cost" => 0
+    ],
+
+];
